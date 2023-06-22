c4886d0c033b8c158e3022fa7e7a685f9369a60d
Jace Gonzales <jacegonzales25@gmail.com>
2023-06-22 12:26:38 +0800

    Added structural system

---
+++ share_prompts_jsmastery/app/posts/new/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// example of nested routing
+
+// posted at posts/new
+
+export default function NewPost(){
+    return (
+        <div>
+            <h1>New Post</h1>
+        </div>
+    )
+}
